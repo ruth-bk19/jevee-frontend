dece2b3fb9119598663d1753f9d87e50377e6f29
ruth-bk19 <ruth.077@godawari.edu.np>
2024-11-26 09:08:51 +0545

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,8 @@ import products from "../data/product";
 export default function Home() {
   return (
     <>
-      <Header />
+
+      
       <h1 className="text-slate-800 md:text-orange-600 lg:text-indigo-600">
         THis is Home page
       </h1>
